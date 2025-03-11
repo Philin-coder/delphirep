559216509375832c1203482bb1180b5c8fd08f54
Alexey <80147483+Philin-coder@users.noreply.github.com>
2025-03-11 15:17:38 +0300

    add fn_unit

--- code/cont_r/cont_project.dpr
+++ code/cont_r/cont_project.dpr
@@ -2,12 +2,13 @@ program cont_project;
 
 uses
   Forms,
-  Un_main in 'Un_main.pas' {Form2};
+  Un_main in 'Un_main.pas' {Form2},
+  Un_func in 'Un_func.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TFrm_main,  TFrm_main);
   Application.Run;
 end.
