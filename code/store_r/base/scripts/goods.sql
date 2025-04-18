0b9caa7f3190c312c684574792fe5e50e653a4ce
Alexey <80147483+Philin-coder@users.noreply.github.com>
2025-04-18 14:55:27 +0300

    goods fixed

--- code/store_r/base/scripts/goods.sql
+++ code/store_r/base/scripts/goods.sql
@@ -1,7 +1,7 @@
 USE [store_r]
 GO
 
-/****** Object:  Table [dbo].[goods]    Script Date: 17.04.2025 22:45:48 ******/
+/****** Object:  Table [dbo].[goods]    Script Date: 18.04.2025 14:54:07 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,7 +11,11 @@ GO
 CREATE TABLE [dbo].[goods](
 	[GoodsId] [int] NOT NULL,
 	[goods_naim] [varchar](150) NOT NULL,
-	[goods_cost] [decimal](10, 2) NOT NULL
+	[goods_cost] [decimal](10, 2) NOT NULL,
+ CONSTRAINT [PK_goods] PRIMARY KEY CLUSTERED 
+(
+	[GoodsId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
