fe227d28c980ed6fef057730a407d0935ec23a3f
Alexey <80147483+Philin-coder@users.noreply.github.com>
2025-05-07 15:30:11 +0300

    ins_nom

--- code/hotel_r/base/StoredProc/sel_nom_by_kind.sql
+++ code/hotel_r/base/StoredProc/sel_nom_by_kind.sql
@@ -1,19 +1,19 @@
 USE [hotel_r]
 GO
 
-/****** Object:  StoredProcedure [dbo].[sel_nom_by_kind]    Script Date: 07.05.2025 9:54:04 ******/
+/****** Object:  StoredProcedure [dbo].[sel_nom_by_kind]    Script Date: 07.05.2025 15:23:21 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-create proc [dbo].[sel_nom_by_kind]
+CREATE proc [dbo].[sel_nom_by_kind]
 @nomer_kind varchar(1000)
 as
 begin
 select 
-nomer.id_type_nomer,
+nomer.nomer_id,
 nomer.nomer_kind,
 nomer.nomer_cost_day,
 type_nomer.type_image
