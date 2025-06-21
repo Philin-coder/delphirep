5bc28c9e95be207e42f5f7c96b40cad83194a21f
Alexey <80147483+Philin-coder@users.noreply.github.com>
2025-06-21 23:18:59 +0300

    saver_init

---
+++ code/saver_r/saver_project.dpr
@@ -0,0 +1,17 @@
+program saver_project;
+
+uses
+  Forms,
+  Un_func in 'Un_func.pas',
+  Un_main in 'Un_main.pas' {Frm_main},
+  Un_dm in 'Un_dm.pas' {DataModule1: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFrm_main, Frm_main);
+  Application.CreateForm(TFrm_main, Frm_main);
+  Application.CreateForm(TDataModule1, DataModule1);
+  Application.Run;
+end.
